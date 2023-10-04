6071d73b42beaecce55a71b7cc52de9bbbbc7131
Daniel Camilo Montoya <dacamoty@gmail.com>
2023-10-04 15:34:21 -0500

    feat(renovate): update project to node18 (#354)
    Co-authored-by: Daniel Montoya <daniel.montoya@eclass.cl>

--- react-app.js
+++ react-app.js
@@ -1,17 +1,17 @@
 module.exports = {
   extends: [
-    'standard',
-    'react-app',
-    'prettier',
-    'prettier/flowtype',
-    'prettier/react',
-    'prettier/standard'
+    "standard",
+    "react-app",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react",
+    "prettier/standard"
   ],
   rules: {
-    '@typescript-eslint/consistent-type-assertions': 'off',
-    '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
-    'jsx-a11y/href-no-hash': [0],
-    'no-console': ['error'],
-    'react-hooks/exhaustive-deps': 'off'
+    "@typescript-eslint/consistent-type-assertions": "off",
+    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
+    "jsx-a11y/href-no-hash": [0],
+    "no-console": ["error"],
+    "react-hooks/exhaustive-deps": "off"
   }
-}
+};
